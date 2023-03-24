aee2a7d28aec0fd52cd9e713e3f8320826ee03a8
CarlGauss1 <eharty1@outlook.com>
2023-03-24 20:14:37 +0000

    Added sizes to product model and size selctor box to template

--- kris_kringle/settings.py
+++ kris_kringle/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', ')gt^du0*+70z_o*2yto7s46mq5qny7y#vx5gwzs(_0((i0bfya')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['kriskringle.herokuapp.com', 'localhost']
 

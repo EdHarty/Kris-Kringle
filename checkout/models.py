4dc32fe60a567056a531f39739aa4ebf5e7e241f
CarlGauss1 <eharty1@outlook.com>
2023-03-26 08:21:31 +0000

    Finished checkout model

--- checkout/models.py
+++ checkout/models.py
@@ -25,16 +25,17 @@ class Order(models.Model):
 
     def _generate_order_number(self):
         """
-        Generate a random, unique order number using UUID
+        Generate a unique random order number using UUID
         """
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
         """
         Update grand total each time a line item is added,
-        accounting for delivery costs.
+        including delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))
+        ['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -44,8 +45,8 @@ class Order(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the order number
-        if it hasn't been set already.
+        Override original save method to set the order number
+        if it hasn't been already set.
         """
         if not self.order_number:
             self.order_number = self._generate_order_number()
@@ -64,7 +65,7 @@ class OrderLineItem(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the lineitem total
+        Override original save method to set the lineitem total
         and update the order total.
         """
         self.lineitem_total = self.product.price * self.quantity

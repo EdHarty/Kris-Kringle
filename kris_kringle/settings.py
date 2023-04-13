5c30322addf9634c74832c1f95bf0c40f57afbbe
CarlGauss1 <eharty1@outlook.com>
2023-04-13 10:12:54 +0000

    Added confirmation emails

--- kris_kringle/settings.py
+++ kris_kringle/settings.py
@@ -187,6 +187,7 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'kriskringle@example.com'
 
 if 'USE_AWS' in os.environ:
     # Cache control

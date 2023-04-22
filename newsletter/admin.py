59472bb4fca9e33737d733f014d5f1e396d83604
CarlGauss1 <eharty1@outlook.com>
2023-04-22 14:15:51 +0000

    Edited newsletter admin

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import UserContact, NewsLetter
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'name_surname', 'email', 'details',)
+
+    search_fields = ('name_surname', 'email')
+
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = (
+        'date', 'email',)
+
+    search_fields = ('date', 'email',)
+
+
+admin.site.register(NewsLetter, NewsletterAdmin)
+admin.site.register(UserContact, ContactAdmin)

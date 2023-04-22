3f85cdaa46b45c1ed6dbe8080480c11ebd77263e
CarlGauss1 <eharty1@outlook.com>
2023-04-22 13:56:50 +0000

    Added newsletter and contact model

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class UserContact(models.Model):
+    """ A model for user contact form """
+
+    name_surname = models.CharField(max_length=200, null=False, blank=False)
+    email = models.EmailField(max_length=200, null=False, blank=False)
+    details = models.CharField(max_length=55, null=False, blank=False)
+    enquiry = models.TextField(max_length=220, null=False, blank=False)
+
+
+class NewsLetter(models.Model):
+    """ A model for subscription to newsletter """
+
+    date = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return self.email

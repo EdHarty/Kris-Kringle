66e3defe375077fd3364920fb45098ee14d6bc46
CarlGauss1 <eharty1@outlook.com>
2023-04-26 23:06:06 +0000

    set debug

--- kris_kringle/settings.py
+++ kris_kringle/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', ')gt^du0*+70z_o*2yto7s46mq5qny7y#vx5gwzs(_0((i0bfya')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kriskringle.herokuapp.com', 'localhost']
 

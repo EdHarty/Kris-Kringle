059bb05c4ef80ca8bd6c1e06d2825b466e4343f5
CarlGauss1 <eharty1@outlook.com>
2023-05-03 13:11:38 +0000

    Deployment

--- kris_kringle/settings.py
+++ kris_kringle/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', ')gt^du0*+70z_o*2yto7s46mq5qny7y#vx5gwzs(_0((i0bfya')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kriskringle.herokuapp.com', 'localhost']
 
@@ -132,10 +132,6 @@ else:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-    }
-
-    DATABASES = {
-    'default': dj_database_url.parse('postgres://wpqhcgfy:pBnzRlDEiK68ynKAInmGf9G5Xbudsj2G@mel.db.elephantsql.com/wpqhcgfy')
     }
 
 
